ae3a26c535211e1c13197a446d7309fd193c6343
Himsh1216 <140539171+Himsh1216@users.noreply.github.com>
2024-11-24 03:55:23 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -175,7 +175,7 @@ const HomePage = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          to: '24me01040@iitbbs.ac.in', // Replace with the admin's email
+          to: '24me01040@iitbbs.ac.in', 
           subject: 'New NSS Volunteer Registration',
           content: emailContent,
         }),
